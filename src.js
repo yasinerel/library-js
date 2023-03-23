979ec7d36ee4812e30c953a00f5a3da684b3959b
yasinerel <terel19@ku.edu.tr>
2023-03-23 17:11:50 +0300

    interface done

---
+++ src.js
@@ -0,0 +1,19 @@
+let myLibrary = [];
+
+function Book(name, author, page, read) {
+    this.name = name;
+    this.author = author;
+    this.page = page;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+  const newBook = new Book('yasin', 'erel', 69, true);
+  myLibrary.push(newBook);
+}
+
+addBookToLibrary();
+
+console.log("deneme");
+console.log(myLibrary);
