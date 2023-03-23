b6168a97fb330c92ff167ea0f96c3e7a54575028
yasinerel <terel19@ku.edu.tr>
2023-03-23 22:22:53 +0300

    general structur done

--- src.js
+++ src.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+/*let myLibrary = [];
 
 function Book(name, author, page, read) {
     this.name = name;
@@ -8,11 +8,33 @@ function Book(name, author, page, read) {
 }
 
 function addBookToLibrary() {
-  // do stuff here
   const newBook = new Book('yasin', 'erel', 69, true);
   myLibrary.push(newBook);
 }
 
 addBookToLibrary();
 console.log("deneme");
-console.log(myLibrary);
+console.log(myLibrary);*/
+
+
+let myLibrary = [];
+
+function Book(name, author, page, read) {
+  this.name = name;
+  this.author = author;
+  this.page = page;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  const name = document.getElementById("bookName").value;
+  const author = document.getElementById("authorName").value;
+  const page = document.getElementById("pageNumber").value;
+  const read = document.getElementById("read").checked;
+
+  const newBook = new Book(name, author, page, read);
+  myLibrary.push(newBook);
+
+  console.log(myLibrary);
+}
+
